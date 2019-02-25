d616a8e1826b0a82bb203ab1dec34ffcb4921c8f
Michael <print@3daddict.com>
2019-02-24 16:44:43 -0800

    basic get method demo res with json

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+//Get Method
+app.get('/greetings', (req, res) => {
+    //respond with json object
+    res.json({greeting: "Hello World!"});
+});
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
