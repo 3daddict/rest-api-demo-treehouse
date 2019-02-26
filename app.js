8da5026464838f90eb755f22b373a936ff5a963f
Michael <print@3daddict.com>
2019-02-25 19:09:11 -0800

    update quote method created

--- app.js
+++ app.js
@@ -51,6 +51,22 @@ app.post("/quotes", async (req, res) => {
 });
 
 //Send a PUT request to /quotes/:id UPDATE (edit) a quote
+app.put('/quotes/:id', async (req, res) => {
+    try {
+        const quote = await records.getQuote(req.params.id);
+        if (quote) {
+            quote.quote = req.body.quote;
+            quote.author = req.body.author;
+
+            await records.updateQuote(quote);
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: "Quote not found..." })
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
 
 //Send a DELETE request to /quotes/:id Delete a quote
 
